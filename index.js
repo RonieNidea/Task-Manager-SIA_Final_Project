5a1d236cc9e5389c78221b1c24efca9b124e9dea
RonieNidea <ronienidea.202301639@gmail.com>
2026-05-23 22:23:39 +0800

    commit init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
@@ -13,6 +14,7 @@ const supabase = createClient(
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 /*
 =====================================
